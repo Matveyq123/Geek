5eb2a722a556db8bb96f4636ab56f03f3c59e4bc
Matvii Korobieinikov <matvii.korobieinikov@globallogic.com>
2021-09-21 12:23:17 +0300

    Main added

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <exception>
+#include<cctype>
+#include <algorithm>
+
+int main()
+{
+	std::string str = "DYna.Co.loR.x.";
+	std::string cameraType = "D.Yna.COL.Or>..";
+	std::for_each(cameraType.begin(), cameraType.end(), [&cameraType]() 
+	{
+		auto it = cameraType.begin();
+		while (it != cameraType.end())
+		{
+			if (isalpha(*it))
+			{
+				*it = std::tolower(*it);
+			}
+			else
+			{
+				cameraType.erase(it);
+				--it;
+				std::cout << cameraType << std::endl;
+			}
+			++it;
+		}
+
+	});
+	auto it = str.begin();
+	while(it != str.end())
+	{
+		if (isalpha(*it))
+		{
+			*it = std::tolower(*it);
+		}
+		else
+		{
+			std::cout << str << std::endl;
+			str.erase(it);
+			--it;
+			std::cout << str << std::endl;
+		}
+		++it;
+	}
+
+	return 0;
+}
